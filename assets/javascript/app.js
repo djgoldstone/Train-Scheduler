a3306e3a33ccb162746e8ca559abdaf49caae535
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-08 17:10:14 -0700

    Added bootstrap jumbotron containing header of train tracker hw. Added two bootstrap cards; top card containing a table with colums for train name, destination, frequency, next arrival time, and minutes away. Bottom card containing input fields to add a new train by: name, destination, first train time, and frequency with a submit button. Added corresponsing div id's.

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyBPgbt8VE6gJiRBV5B843U3sj_K9Pb2BZE",
+    authDomain: "derek-s-project.firebaseapp.com",
+    databaseURL: "https://derek-s-project.firebaseio.com",
+    projectId: "derek-s-project",
+    storageBucket: "derek-s-project.appspot.com",
+    messagingSenderId: "998895681133",
+    appId: "1:998895681133:web:c5c94cf966efc973"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
