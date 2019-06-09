07e0c56d3a236a29b929d5b10d3d8f1cb355f09c
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-09 11:46:40 -0700

    Accessed firebase database and stored input from new train form/displayed to DOM using JQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,33 @@ var config = {
   firebase.initializeApp(config);
 
   var database = firebase.database();
+
+  $("#add-train-btn").on("click", function(event) {
+      event.preventDefault();
+
+      var trainName = $("#train-name-input").val().trim();
+      var destination = $("#destination-input").val().trim();
+      var firstTrainTime = $("#first-train-input").val().trim();
+      var frequency = $("#frequency-input").val().trim();
+
+      var newTrain = {
+          trainName: trainName,
+          destination: destination,
+          firstTrainTime: firstTrainTime,
+          frequency: frequency
+      };
+
+      database.ref().push(newTrain);
+  });
+
+  database.ref().on("child_added", function(snapshot) {
+      var child = snapshot.val();
+      var newRow = $("<tr>");
+      var trainNameData = $("<td>").text(child.trainName);
+      var destinationData = $("<td>").text(child.destination);
+      var firstTrainTimeData = $("<td>").text(child.firstTrainTime);
+      var frequencyData = $("<td>").text(child.frequency);
+
+      $("#table").append(newRow, trainNameData, destinationData, firstTrainTimeData, frequencyData); }, function (errorObject) {
+          console.log("Errors handled: " + errorObject.code);
+  });
